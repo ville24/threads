5c86a58b1c5a8b59c711a4cd18d78e4f8eb5695f
ville24 <73233385+ville24@users.noreply.github.com>
2024-11-21 23:32:48 +0200

    Update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -3,31 +3,33 @@ const {LoggingWinston} = require('@google-cloud/logging-winston')
 
 const loggingWinston = new LoggingWinston()
 
-if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
+/*
+ *if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
+ *
+ *    module.exports = winston.createLogger({
+ *      transports:
+ *          new winston.transports.File({filename: 'logs/server.log',
+ *            format: winston.format.combine(
+ *              winston.format.timestamp({format: 'MMM-DD-YYYY HH:mm:ss'}),
+ *              winston.format.align(),
+ *              winston.format.printf((info) => `${info.level}: ${[info.timestamp]}: ${info.message}`)
+ *            )})
+ *    })
+ *
+ *} else {
+ */
 
-  module.exports = winston.createLogger({
-    transports:
-        new winston.transports.File({filename: 'logs/server.log',
-          format: winston.format.combine(
-            winston.format.timestamp({format: 'MMM-DD-YYYY HH:mm:ss'}),
-            winston.format.align(),
-            winston.format.printf((info) => `${info.level}: ${[info.timestamp]}: ${info.message}`)
-          )})
-  })
+module.exports = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp({format: 'MMM-DD-YYYY HH:mm:ss'}),
+    winston.format.align(),
+    winston.format.printf((info) => `${info.level}: ${[info.timestamp]}: ${info.message}`)
+  ),
+  transports: [
+    new winston.transports.Console(),
+    loggingWinston
+  ]
+})
 
-} else {
-
-  module.exports = winston.createLogger({
-    level: 'info',
-    format: winston.format.combine(
-      winston.format.timestamp({format: 'MMM-DD-YYYY HH:mm:ss'}),
-      winston.format.align(),
-      winston.format.printf((info) => `${info.level}: ${[info.timestamp]}: ${info.message}`)
-    ),
-    transports: [
-      new winston.transports.Console(),
-      loggingWinston
-    ]
-  })
-
-}
+// }
