19a0c9e082be8ed09bb42ba112e4b363990fa6f5
ville24 <73233385+ville24@users.noreply.github.com>
2024-11-22 11:24:47 +0200

    Update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -1,21 +1,25 @@
 const winston = require('winston')
 const {LoggingWinston} = require('@google-cloud/logging-winston')
 
-const loggingWinston = new LoggingWinston()
-
 if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
 
-  module.exports = winston.createLogger({
-    transports:
-        new winston.transports.File({filename: 'logs/server.log',
-          format: winston.format.combine(
-            winston.format.timestamp({format: 'MMM-DD-YYYY HH:mm:ss'}),
-            winston.format.align(),
-            winston.format.printf((info) => `${info.level}: ${[info.timestamp]}: ${info.message}`)
-          )})
-  })
+  try {
+
+    module.exports = winston.createLogger({
+      transports:
+          new winston.transports.File({filename: 'logs/server.log',
+            format: winston.format.combine(
+              winston.format.timestamp({format: 'MMM-DD-YYYY HH:mm:ss'}),
+              winston.format.align(),
+              winston.format.printf((info) => `${info.level}: ${[info.timestamp]}: ${info.message}`)
+            )})
+    })
+
+  } catch {}
 
 } else {
+
+  const loggingWinston = new LoggingWinston()
 
   module.exports = winston.createLogger({
     level: 'info',
