44d7fe86cbc9dae886e9be329345ff2abfc5ee54
ville24 <73233385+ville24@users.noreply.github.com>
2024-11-22 12:00:49 +0200

    Update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -1,37 +1,39 @@
-const winston = require('winston')
-const {LoggingWinston} = require('@google-cloud/logging-winston')
-
-if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
-
-  try {
-
-    module.exports = winston.createLogger({
-      transports:
-          new winston.transports.File({filename: 'logs/server.log',
-            format: winston.format.combine(
-              winston.format.timestamp({format: 'MMM-DD-YYYY HH:mm:ss'}),
-              winston.format.align(),
-              winston.format.printf((info) => `${info.level}: ${[info.timestamp]}: ${info.message}`)
-            )})
-    })
-
-  } catch {}
-
-} else {
-
-  const loggingWinston = new LoggingWinston()
-
-  module.exports = winston.createLogger({
-    level: 'info',
-    format: winston.format.combine(
-      winston.format.timestamp({format: 'MMM-DD-YYYY HH:mm:ss'}),
-      winston.format.align(),
-      winston.format.printf((info) => `${info.level}: ${[info.timestamp]}: ${info.message}`)
-    ),
-    transports: [
-      new winston.transports.Console(),
-      loggingWinston
-    ]
-  })
-
-}
+/*
+ *const winston = require('winston')
+ *const {LoggingWinston} = require('@google-cloud/logging-winston')
+ *
+ *if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
+ *
+ *  try {
+ *
+ *    module.exports = winston.createLogger({
+ *      transports:
+ *          new winston.transports.File({filename: 'logs/server.log',
+ *            format: winston.format.combine(
+ *              winston.format.timestamp({format: 'MMM-DD-YYYY HH:mm:ss'}),
+ *              winston.format.align(),
+ *              winston.format.printf((info) => `${info.level}: ${[info.timestamp]}: ${info.message}`)
+ *            )})
+ *    })
+ *
+ *  } catch {}
+ *
+ *} else {
+ *
+ *  const loggingWinston = new LoggingWinston()
+ *
+ *  module.exports = winston.createLogger({
+ *    level: 'info',
+ *    format: winston.format.combine(
+ *      winston.format.timestamp({format: 'MMM-DD-YYYY HH:mm:ss'}),
+ *      winston.format.align(),
+ *      winston.format.printf((info) => `${info.level}: ${[info.timestamp]}: ${info.message}`)
+ *    ),
+ *    transports: [
+ *      new winston.transports.Console(),
+ *      loggingWinston
+ *    ]
+ *  })
+ *
+ *}
+ */
