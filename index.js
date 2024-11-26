035e1c47cec7d7cba6d981b01956a8628c02ba42
ville24 <73233385+ville24@users.noreply.github.com>
2024-11-26 12:16:09 +0200

    tested dev and prod

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ const config = require('./utils/config')
 
 const server = http.createServer(app)
 
+const PORT = process.env.PORT || config.PORT
 server.listen(
-  config.PORT,
+  PORT,
   () => {
 
-    console.log(`Server running on port ${config.PORT}`)
-    // logger.info(`Server running on port ${config.PORT}`)
+    console.log(`Server running on port ${PORT}`)
+    // logger.info(`Server running on port ${PORT}`)
 
   }
 )
