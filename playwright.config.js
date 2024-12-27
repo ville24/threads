85add990e7cb2a5b6c730950e96dfe89587a4a9e
ville24 <73233385+ville24@users.noreply.github.com>
2024-12-27 14:02:52 +0200

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -11,7 +11,7 @@ const {defineConfig, devices} = require('@playwright/test')
  * @see https://playwright.dev/docs/test-configuration
  */
 module.exports = defineConfig({
-  testDir: './e2e-tests',
+  testDir: './e2e-tests2',
 
   /* Run tests in files in parallel */
   fullyParallel: true,
