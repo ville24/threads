8ec15f3da7ae22ba4d19e5e96aa7160fd92d051c
ville24 <73233385+ville24@users.noreply.github.com>
2024-12-28 11:40:41 +0200

    fix

--- playwright.config.js
+++ playwright.config.js
@@ -11,13 +11,13 @@ const {defineConfig, devices} = require('@playwright/test')
  * @see https://playwright.dev/docs/test-configuration
  */
 module.exports = defineConfig({
-  testDir: 'e2e-tests',
+  testDir: './e2e-tests',
 
   /* Run tests in files in parallel */
   fullyParallel: true,
 
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  // forbidOnly: !!process.env.CI,
+  forbidOnly: !!process.env.CI,
 
   /* Retry on CI only */
   retries: process.env.CI
