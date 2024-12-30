3d633f0b63a1a4f43323c6c09f648a45c7cfe4ce
ville24 <73233385+ville24@users.noreply.github.com>
2024-12-30 12:45:28 +0000

    fix

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const http = require('http')
 
 const app = require('./app')
-
-// const config = require('./utils/config')
-
+const config = require('./utils/config')
 // const logger = require('./utils/logger')
 
 const server = http.createServer(app)
 
-const PORT = process.env.PORT || 3004
+const PORT = process.env.PORT || config.PORT || 8081
 server.listen(
   PORT,
   () => {
