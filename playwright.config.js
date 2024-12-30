bd3af02491f710db6c4dbfbfd0313358c114af92
ville24 <73233385+ville24@users.noreply.github.com>
2024-12-30 10:28:09 +0000

    fix3

--- playwright.config.js
+++ playwright.config.js
@@ -75,7 +75,7 @@ module.exports = defineConfig({
   use: {
 
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3004',
+    baseURL: 'http://localhost:3004',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry'
