78518a6688083cbfe43d2e56fb1e80eca39d83fe
ville24 <73233385+ville24@users.noreply.github.com>
2024-12-30 13:00:08 +0000

    fix

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const config = require('./utils/config')
 
 const server = http.createServer(app)
 
-const PORT = process.env.PORT || config.PORT || 3004
+const PORT = process.env.PORT || config.PORT || 8081
 server.listen(
   PORT,
   () => {
